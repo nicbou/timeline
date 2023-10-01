01f1f0d1a080482648ae88613ca65a4dcac7e9a8
Nicolas Bouliane <contact@nicolasbouliane.com>
2023-10-01 13:05:38 +0200

    Better support for multi-day calendar events

--- timeline/templates/js/components/entries/event.js
+++ timeline/templates/js/components/entries/event.js
@@ -8,20 +8,42 @@ export default Vue.component('event-entry', {
     }
   },
   computed: {
-    dateStart: function() {
-      return new Date(this.entry.date_start);
+    currentDate() {
+      return moment(this.$route.query.date, 'YYYY-MM-DD', true);
     },
-    dateEnd: function() {
-      return this.entry.date_end ? new Date(this.entry.date_end) : null;
+    isAllDay() {
+      return (
+        this.dateStart <= this.currentDate.startOf('day')
+        && this.dateEnd
+        && this.dateEnd >= this.currentDate.endOf('day').milliseconds(0)
+      );
     },
-    description: function() {
+    startsEarlier() {
+      return this.dateStart < this.currentDate.startOf('day');
+    },
+    endsLater() {
+      return this.dateEnd && this.dateEnd > this.currentDate.endOf('day').milliseconds(0);
+    },
+    dateStart() {
+      return moment(this.entry.date_start);
+    },
+    dateEnd() {
+      return this.entry.date_end ? moment(this.entry.date_end) : null;
+    },
+    dateEndPrint() {
+      // Adds the missing second to the end date
+      return moment(this.dateEnd).add(1, 'seconds');
+    },
+    description() {
       return (this.entry.data.description || '').trim().replaceAll('<br><br>', '<br>').replaceAll('\n', '<br>');
     }
   },
   template: `
-    <article class="entry event" @click="expanded = !expanded" :class="{'expanded': expanded}">
+    <article class="entry event" @click="expanded = !expanded" :class="{'expanded': expanded, 'starts-earlier': startsEarlier, 'ends-later': endsLater}">
       <i class="fa-regular fa-calendar"></i>
-      <time>{{ dateStart.toLocaleTimeString('en-GB', {hour: 'numeric', minute:'2-digit'}) }}</time>
+      <time v-if="!isAllDay && !startsEarlier">{{ dateStart.format('H:mm') }}</time>
+      <time v-if="!isAllDay && startsEarlier">&hellip;{{ dateEndPrint.format('H:mm') }}</time>
+      <time class="all-day" v-if="isAllDay">All day</time>
       <div class="summary">{{ entry.data.summary.replaceAll('\\n', ', ') }}</div>
       <div class="description" v-if="description" v-html="description"></div>
     </article>
