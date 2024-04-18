43a380fd8ba38eff754acdc74f31714d7ceeea3f
Nicolas Bouliane <contact@nicolasbouliane.com>
2024-04-18 09:30:56 +0200

    Geolocation map: nicer polyline, add end marker

--- timeline/templates/js/components/google-map.js
+++ timeline/templates/js/components/google-map.js
@@ -9,6 +9,8 @@ export default Vue.component('google-map', {
     return {
       map: null,
       polyline: null,
+      polylineShadow: null,
+      endMarker: null,
       currentMapFeatures: [],
     };
   },
@@ -21,13 +23,41 @@ export default Vue.component('google-map', {
       if (!this.map) { return }
       if (this.polyline) {
         this.polyline.setMap(null);
+        this.polylineShadow.setMap(null);
       }
+
+      this.polylineShadow = new google.maps.Polyline({
+        path: this.markers,
+        geodesic: true,
+        strokeOpacity: 0.7,
+        strokeColor: '#fff',
+        strokeWeight: 6,
+        map: this.map,
+      });
       this.polyline = new google.maps.Polyline({
         path: this.markers,
         geodesic: true,
-        strokeOpacity: 1.0,
-        strokeWeight: 2,
+        strokeOpacity: 1,
+        strokeColor: '#64719c',
+        strokeWeight: 3,
         map: this.map,
+      });
+
+      if (this.endMarker) {
+        this.endMarker.setMap(null);
+      }
+      this.endMarker = new google.maps.Marker({
+        icon: {
+          path: google.maps.SymbolPath.CIRCLE,
+          fillColor: '#64719c',
+          fillOpacity: 1,
+          strokeWeight: 1.5,
+          strokeColor: '#fff',
+          strokeOpacity: 0.9,
+          scale: 5,
+        },
+        map: this.map,
+        position: this.markers[this.markers.length - 1],
       });
 
       const mapBounds = new google.maps.LatLngBounds();
